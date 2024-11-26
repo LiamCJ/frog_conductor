73bac9bbc5fa116742c9af1494c7ec5c52cbff03
oli-the-broccoli <oliver@hopegf.com>
2024-11-26 22:46:21 +0200

    Updated lvl 1
    lvl 1 is just being used for testing still

--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 var speed:int = 100
-@onready var path_follow_2d:PathFollow2D = $tram_paths/Path2D/PathFollow2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,9 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:float) -> void:
-	var len:float = $tram_paths/Path2D.curve.get_baked_length()
-	path_follow_2d.progress_ratio += 0.01 # (speed * delta)/len
-	print(path_follow_2d.position)
-	$Tram2.position = path_follow_2d.position
+	pass
+	#var len:float = $tram_paths/Path2D.curve.get_baked_length()
+	#path_follow_2d.progress_ratio += 0.01 # (speed * delta)/len
 
-	
