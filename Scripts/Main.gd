cc6e2273a131173bf6131a3e1037a257c0bba951
oli-the-broccoli <oliver@hopegf.com>
2024-12-01 00:09:19 +0200

    Animal logic
    Added random spawning of animals
    kill animals when tram hits them

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,7 +4,9 @@ extends Node2D
 @export var next_level:PackedScene
 @export var levels: Array[PackedScene]
 @onready var tram_controller: Node = $TramController
-@export var animal:PackedScene
+@export var animals: Array[PackedScene]
+var top_animals: Array[Area2D]
+var bottom_animals: Array[Area2D]
 @export var Coin:PackedScene
 
 #player properties
@@ -34,19 +36,39 @@ func load_next_level()-> void:
 	tram_controller.init_network()
 	tram_controller.update_tram = true
 	
+func on_animal_hit(animal:Area2D) ->void:
+	animal.kill()
+	if animal == top_animals[0] or animal == bottom_animals[0]:
+		tram_controller.speed = 0
+
 func spawn_animals()->void:
-	var animal_spawns: Node = level.animal_spawns
-	for point: Marker2D in animal_spawns.top_path:
-		var an: = animal.instantiate()
-		add_child(an)
-		an.reparent(level)
-		an.position = point.position
+	top_animals.clear()
+	bottom_animals.clear()
 	
-	#testing coins
-	#for point: Marker2D in animal_spawns.bottom_path:
-		#var coin:Node2D = Coin.instantiate()
-		#add_child(coin)
-		#coin.position = point.position
-		#coin.value = 20
+	var animal_markers: Node = level.animal_spawns
 	
-	
+	#choose random number of animals to spawn
+	var num_top:int = randi_range(1,animal_markers.top_path.size())
+	for i:int in range(num_top):
+		#choose random animal
+		var animal_idx:int = randi_range(0,animals.size() - 1)
+		var new_animal:Area2D = animals[animal_idx].instantiate()
+		#set position of animal to marker
+		new_animal.position = animal_markers.top_path[i].position
+		
+		
+		level.add_child(new_animal) #animals added to level so they are freed with it
+		#note: cant access children (coin) before setting parent
+		new_animal.set_value(randi_range(1,9)*10)
+		top_animals.append(new_animal)
+
+	var num_bottom:int = randi_range(1,animal_markers.bottom_path.size())
+	for i:int in range(num_bottom):
+		var animal_idx:int = randi_range(0,animals.size() - 1)
+		var new_animal:Area2D = animals[animal_idx].instantiate()
+		new_animal.position = animal_markers.bottom_path[i].position
+		level.add_child(new_animal)
+		new_animal.set_value(randi_range(1,9)*10)
+		bottom_animals.append(new_animal)
+
+
